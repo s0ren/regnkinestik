6bf475d8c42d2e0b29bff1e250fb5e290a64859a
New123User321 <Ny123Mail321@gmail.com>
2019-03-11 13:02:52 +0100

    Add files via upload

---
+++ kap2/rotation.m
@@ -0,0 +1,21 @@
+function m = rotation(psi, phi, theta)
+Rx = [
+        [1 0          0]; 
+        [0 cos(phi) -sin(phi)];
+        [0 sin(phi) cos(phi)]
+    ];
+
+Ry = [
+        [cos(theta)  0 sin(theta)];
+        [0           1 0];
+        [-sin(theta) 0 cos(theta)]
+];
+
+Rz = [
+        [cos(psi) -sin(psi) 0];
+        [sin(psi) cos(psi)  0];
+        [0        0         1]
+];
+
+m = Rz * Rx * Ry;
+end
